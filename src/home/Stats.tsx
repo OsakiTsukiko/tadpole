3649c8038e187aa1331dfb83579314f090811d18
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-03-27 13:05:16 +0200

    add chart

--- src/home/Stats.tsx
+++ src/home/Stats.tsx
@@ -1,5 +1,6 @@
 import '../scss/styles.css'
 import { useTadpoles } from '../TadpoleProvider';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 function Stats() {
     const { currentTP, tadpoles } = useTadpoles();
@@ -8,18 +9,32 @@ function Stats() {
         b.seedlings.length - a.seedlings.length
     );
 
+    const chartData = filteredTadpoles.map(tp => ({
+        name: tp.username,
+        value: tp.seedlings.length,
+    }));
+
+
     return (
         <div className='stats'>
             <div className='list'>
                 {filteredTadpoles.map(tp =>
                     <div className='element'>
                         <span className='username roboto'>{tp.username}</span>
-                        <span className='value roboto'>{tp.seedlings.length}</span>
+                        <span className='value roboto'>{tp.seedlings.length} Seedlings</span>
                     </div>
                 )}
             </div>
             <div className='chart'>
-
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart data={chartData}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Bar dataKey="value" fill="#A4D48C" />
+                    </BarChart>
+                </ResponsiveContainer>
             </div>
         </div>
     )
